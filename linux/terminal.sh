b5e2af01fba3cd1d3bfb6fc4649efcb7b94afab3
0xluckycoder <lakshanperera.dev@gmail.com>
2025-06-19 23:29:30 +0530

    update terminal to accept commands in new line

--- linux/terminal.sh
+++ linux/terminal.sh
@@ -4,12 +4,15 @@ SESSION_NAME="lakshan-tmux-session"
 DIR="$HOME/projects"
 
 tmux new-session -d -s $SESSION_NAME
+tmux send-keys -t $SESSION_NAME:0.0 "export PS1=\"\u@\h:\w\n🌵$ \"" C-m
 tmux send-keys -t $SESSION_NAME:0.0 "cd $DIR" C-m
 tmux send-keys -t $SESSION_NAME:0.0 "clear" C-m
 tmux send-keys -t $SESSION_NAME:0.0 "neofetch" C-m
 tmux split-window -h -t $SESSION_NAME:0.0
+tmux send-keys -t $SESSION_NAME:0.1 "export PS1=\"\u@\h:\w\n🌵$ \"" C-m
 tmux send-keys -t $SESSION_NAME:0.1 "cd $DIR" C-m
 tmux send-keys -t $SESSION_NAME:0.1 "clear" C-m
 tmux split-window -v -t $SESSION_NAME:0.1
+tmux send-keys -t $SESSION_NAME:0.2 "export PS1=\"\u@\h:\w\n🌵$ \"" C-m
 tmux send-keys -t $SESSION_NAME:0.2 'cmatrix' C-m
 tmux attach-session -t $SESSION_NAME
