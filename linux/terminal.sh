08e30a9be30a6643620174a4cc868dc69ca4f29a
0xluckycoder <lakshanperera.dev@gmail.com>
2025-06-25 00:27:27 +0530

    update tmux to show current git branch & lambda symbol

--- linux/terminal.sh
+++ linux/terminal.sh
@@ -4,15 +4,33 @@ SESSION_NAME="lakshan-tmux-session"
 DIR="$HOME/projects"
 
 tmux new-session -d -s $SESSION_NAME
-tmux send-keys -t $SESSION_NAME:0.0 "export PS1=\"\u@\h:\w\n🌵$ \"" C-m
+tmux send-keys -t $SESSION_NAME:0.0 '
+    git_branch() { 
+        git branch 2>/dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/(\1)/"; 
+    }
+' C-m
+tmux send-keys -t $SESSION_NAME:0.0 'export PS1="\w \$(git_branch)\n\[\033[38;5;208m\]λ\[\033[0m\] "' C-m
 tmux send-keys -t $SESSION_NAME:0.0 "cd $DIR" C-m
 tmux send-keys -t $SESSION_NAME:0.0 "clear" C-m
 tmux send-keys -t $SESSION_NAME:0.0 "neofetch" C-m
+
 tmux split-window -h -t $SESSION_NAME:0.0
-tmux send-keys -t $SESSION_NAME:0.1 "export PS1=\"\u@\h:\w\n🌵$ \"" C-m
+tmux send-keys -t $SESSION_NAME:0.1 '
+    git_branch() { 
+        git branch 2>/dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/(\1)/"; 
+    }
+' C-m
+tmux send-keys -t $SESSION_NAME:0.1 'export PS1="\w \$(git_branch)\n\[\033[38;5;208m\]λ\[\033[0m\] "' C-m
 tmux send-keys -t $SESSION_NAME:0.1 "cd $DIR" C-m
 tmux send-keys -t $SESSION_NAME:0.1 "clear" C-m
+
 tmux split-window -v -t $SESSION_NAME:0.1
-tmux send-keys -t $SESSION_NAME:0.2 "export PS1=\"\u@\h:\w\n🌵$ \"" C-m
+tmux send-keys -t $SESSION_NAME:0.2 '
+    git_branch() {
+        git branch 2>/dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/(\1)/"; 
+    }
+' C-m
+tmux send-keys -t $SESSION_NAME:0.2 'export PS1="\w \$(git_branch)\n\[\033[38;5;208m\]λ\[\033[0m\] "' C-m
+tmux send-keys -t $SESSION_NAME:0.2 "clear" C-m
 tmux send-keys -t $SESSION_NAME:0.2 'cmatrix' C-m
 tmux attach-session -t $SESSION_NAME
