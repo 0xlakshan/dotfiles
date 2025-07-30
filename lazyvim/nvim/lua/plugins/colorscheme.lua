b9b2ce3e0fd605ee163725029f19adb19a97617e
0xluckycoder <lakshanperera.dev@gmail.com>
2025-07-30 23:24:04 +0530

    backup Lazyvim config

---
+++ lazyvim/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "craftzdog/solarized-osaka.nvim",
+    branch = "osaka",
+    lazy = false,
+    priority = 1000,
+    opts = function()
+      return {
+        transparent = true,
+      }
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "solarized-osaka",
+    },
+  },
+}
---
+++ lazyvim/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,38 @@
+return {
+  -- tools
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "stylua",
+        "selene",
+        "luacheck",
+        "shellcheck",
+        "shfmt",
+        "tailwindcss-language-server",
+        "typescript-language-server",
+        "css-lsp",
+      })
+    end,
+  },
+
+  -- lsp servers
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        -- Your custom vtsls settings
+        vtsls = {
+          settings = {
+            typescript = {
+              tsserver = {
+                -- Set the memory limit in megabytes
+                maxTsServerMemory = 4096,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
