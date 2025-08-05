fd828453294c0e17daa668d65acde6f5c7d49604
0xluckycoder <lakshanperera.dev@gmail.com>
2025-08-05 20:16:11 +0530

    add noice config

--- lazyvim/nvim/init.lua
+++ lazyvim/nvim/init.lua
@@ -1,2 +1,31 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- npm path for LSP
+vim.env.PATH = vim.env.PATH .. ":/home/lakshan/.nvm/versions/node/v24.5.0/bin"
+
+-- View typescript types on mouse hover (Shift + K)
+-- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+--   border = "rounded",
+-- })
+-- vim.api.nvim_create_autocmd("CursorHold", {
+--   callback = function()
+--     vim.lsp.buf.hover()
+--   end,
+-- })
+-- vim.o.updatetime = 300
+
+require("lspconfig").ts_ls.setup({
+  settings = {
+    typescript = {
+      inlayHints = {
+        includeInlayParameterNameHints = "none", -- Disable parameter name hints
+        includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+      },
+    },
+  },
+})
+
+require("lazy").setup("plugins_lazy.lua")
+
+-- vim.api.nvim_set_hl(0, "LspInlayHint", { Fg = "#585858", bg = "NONE" })
---
+++ lazyvim/nvim/lua/plugins/plugins_lazy.lua
@@ -0,0 +1,24 @@
+return { -- lazy.nvim
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      opts = opts or { routes = {} }
+      table.insert(opts.routes, {
+        filter = {
+          event = "notify",
+          find = "No information available",
+        },
+        opts = { skip = true },
+      })
+    end,
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    },
+  },
+}
