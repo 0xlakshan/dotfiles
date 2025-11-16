86f2e1fa15ac1a1ac10455ad8cf52690556b66d9
0xluckycoder <lakshanperera.dev@gmail.com>
2025-11-16 23:52:04 +0530

    feat: autoamte installation process

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p ~/.local/share/applications
+mkdir -p ~/.config/neofetch
+
+cp ./linux/launch.sh ~/
+cp ./linux/launch.desktop ~/.local/share/applications/
+
+cp ./neofetch/logo.txt ~/
+cp ./neofetch/config.conf ~/.config/neofetch/
